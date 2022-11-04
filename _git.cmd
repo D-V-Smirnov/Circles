ca576ec47e3e064b96aad54b47b16ac002170391
dsv69 <dsv@mail.tu>
2022-11-04 08:08:36 +0100

    first

--- _git.cmd
+++
@@ -1,7 +0,0 @@
-echo "# Zlatibor" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/D-V-Smirnov/Zlatibor.git
-git push -u origin main
